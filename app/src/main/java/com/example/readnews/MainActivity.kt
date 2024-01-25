9e8221388ea4cb7eaac71cc5a7de62de445b10d1
Piyushdhorey <pdhorey@cxr.agency>
2024-01-25 17:21:30 +0530

    Made changes in build.gradle.kts - version downgraded to 17 for kapt splash screen added

--- app/src/main/java/com/example/readnews/MainActivity.kt
+++ app/src/main/java/com/example/readnews/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.readnews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.readnews.ui.theme.ReadNewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ReadNewsTheme {
                 // A surface container using the 'background' color from the theme
@@ -33,7 +37,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxSize()
     )
 }
 
