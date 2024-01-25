0e4ffec1ac374bab8c74d2bbca7e2a6f117fafdb
Piyushdhorey <pdhorey@cxr.agency>
2024-01-25 15:04:49 +0530

    margin changes

--- app/src/main/java/com/example/recipefinder/ui/components/RecipeList.kt
+++ app/src/main/java/com/example/recipefinder/ui/components/RecipeList.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipefinder.data.model.Meal
 
 @Composable
--- app/src/main/java/com/example/recipefinder/ui/components/RecipeListItem.kt
+++ app/src/main/java/com/example/recipefinder/ui/components/RecipeListItem.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -28,6 +27,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -153,3 +153,5 @@ fun getIngredients(meal: Meal): String {
     }
     return ingredients.trimEnd('\n')
 }
+
+
